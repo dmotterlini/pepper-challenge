e7776d99b0b819c6dd628880687509e72b7f29b9
Davide Motte <37443982+dmotte@users.noreply.github.com>
2021-05-19 00:40:11 +0200

    Started to develop LoginForm

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>Login</h1>
+  <p>
+    Welcome back
+  </p>
+  <form @submit="onSubmit">
+    <p>
+      Email*<br />
+      <input type="text" placeholder="mail@somedomain.com" v-model="username" />
+    </p>
+    <p>
+      Password*<br />
+      <input
+        type="password"
+        placeholder="Your secret words"
+        v-model="password"
+      />
+    </p>
+    <input type="submit" value="Login" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+
+      alert(this.username + "\n" + this.password);
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* TODO style */
+</style>
